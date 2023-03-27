9541d550943e35e6a9c173e8ecbf7487c52064fd
henkjurriens70 <henk.jurriens@gmail.com>
2023-03-27 10:19:37 +0200

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
+import { ellipse, pizzaOutline, square, triangle } from 'ionicons/icons';
+import Recipes from './pages/Recipes';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
@@ -42,7 +42,7 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+            <Recipes />
           </Route>
           <Route exact path="/tab2">
             <Tab2 />
@@ -56,8 +56,8 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={pizzaOutline} />
+            <IonLabel>Recepten</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
