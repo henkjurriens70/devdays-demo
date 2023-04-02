ee6f1c6337d511ab64d19bf4a9002e618fccfcbd
henkjurriens70 <henk.jurriens@gmail.com>
2023-04-02 12:13:44 +0200

    improved detail

--- src/components/detail/RecipyDetail.tsx
+++ src/components/detail/RecipyDetail.tsx
@@ -6,7 +6,7 @@ import Preparation from "./Preparation";
 const RecipyDetail: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <img className="w-full" src="/assets/images/bread.png" />
+      <img className="w-full" src="/assets/images/bread.png"  alt="food"/>
       <div className="flex flex-col p-2">
         <div className="font-bold mt-2">Bread</div>
         <div className="my-2">Door Polo</div>
